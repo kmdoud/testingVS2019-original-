8907c99e85802e0d7e0ba89de39ffc4700c0a677
Doud <kdoud@ws.wsfgrp.net>
2019-06-28 15:08:44 -0400

    Add project files.

---
+++ testing2019VS/Print.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testing2019VS
+{
+    public class Print
+    {
+        public int int1 { get; set; }
+        public int int2 { get; set; }
+        public int result { get; set; }
+
+        public int Add()
+        {
+            return result;
+        }
+        public Print()
+        {
+
+        }
+     
+    
+
+    }
+}
+
+
---
+++ testing2019VS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace testing2019VS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var testing = new Print();
+
+            testing.int1 = 5;
+            testing.int2 = 3;
+
+            testing.result = testing.int1 + testing.int2;
+
+            var finished = testing.result;
+
+            Console.WriteLine(finished);
+
+
+        }
+    }
+}
